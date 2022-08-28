d9473f5e754473becc676b7714dd5f18904f1060
Mary Ballantyne <mballantyne777@gmail.com>
2022-08-28 17:01:21 -0600

    refactor and add testing to incomplete create artist for record label

--- app/controllers/record_labels_artists_controller.rb
+++ app/controllers/record_labels_artists_controller.rb
@@ -10,6 +10,14 @@ class RecordLabelsArtistsController < ApplicationController
   end
 
   def create
-    record_label_artist = RecordLabelsArtist.create()
+    @record_label = RecordLabel.find(params[:id])
+    record_label_artist = @record_label.artists.create(artist_params)
+    redirect_to "/record_labels/#{@record_label.id}/artists"
+  end
+
+  private
+
+  def artist_params
+    params.permit(:name, :member_count, :album_count, :actively_touring)
   end
 end
